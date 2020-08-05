fbfedff26f8a563d427a57f97f5c5d8751b80f09
Yani Asari <yani.iman1@gmail.com>
2020-08-05 19:23:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli[0] == nil
+  puts "The line is currently empty."
+end
+end
+
+def take_a_number(katz_deli, other_deli)
+  
+  counter = 0 
+  while counter < names.length
+  katz_deli = names[katz_deli]
+  puts "The line is currently:"
+  counter += 1
+end
+end 
+
+
+
+ 
