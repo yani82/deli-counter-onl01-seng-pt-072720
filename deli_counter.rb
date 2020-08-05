1b801080858c15348ffa8c41e2d192120edb3ae9
Yani Asari <yani.iman1@gmail.com>
2020-08-05 20:59:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(line)
   if line == []
     puts "There is nobody waiting to be served!"
   else 
-    puts "currently serving #{line.shift}."
+    puts "Currently serving #{line.shift}."
   end 
 end 
 
