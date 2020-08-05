178b58cc013f520bd971601e7306ebce27682af3
Yani Asari <yani.iman1@gmail.com>
2020-08-05 20:18:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli[0] == nil
+def line(customer)
+  if customer == []
   puts "The line is currently empty."
-end
 end
 
 def take_a_number(katz_deli, other_deli)
