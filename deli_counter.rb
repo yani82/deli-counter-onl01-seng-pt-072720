1f1a8a843a5ef92629991320d78c9d0baff12e29
Yani Asari <yani.iman1@gmail.com>
2020-08-05 20:59:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(customer)
+  array = []
+  if customer == []
+  puts "The line is currently empty."
+  else 
+  customer.each_with_index do |i, index|
+  message = " #{index+1}. #{i}"
+  array << message 
+  end
+  puts "The line is currently:#{array.join("")}"
+  end
+end 
+
+def take_a_number(line, customer)
+  line << customer 
+  puts "Welcome, #{customer}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line == []
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{line.shift}."
+  end 
+end 
+
+    
